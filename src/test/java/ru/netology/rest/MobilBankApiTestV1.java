f2abad9f5e5a706d36b5b7823ed6f5698d9891b0
Viktor-Omelchenko <90280890+Viktor-Omelchenko@users.noreply.github.com>
2021-12-26 19:47:39 +0300

    commit

---
+++ src/test/java/ru/netology/rest/MobilBankApiTestV1.java
@@ -0,0 +1,30 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class MobilBankApiTestV1 {
+
+    @Test
+
+    void shouldReturnAccounts(){
+
+        given()
+
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .statusCode(200);
+
+
+
+
+
+    }
+
+
+
+
+}
